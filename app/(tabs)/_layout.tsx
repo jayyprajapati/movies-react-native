4023e56a64c234e6171971ae43ea2130be83e45a
Jay Prajapati <jay.prajapati5717@gmail.com>
2025-06-23 08:33:45 +0530

    Feat: add 2 tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,33 @@
+// import { StyleSheet } from 'react-native'
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _layout = () => {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName;
+
+        if (route.name === 'index') {
+          iconName = 'home';
+        } else if (route.name === 'Profile') {
+          iconName = 'user';
+        }
+
+        return <Icon name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: '#007AFF',
+      tabBarInactiveTintColor: 'gray',
+    })}
+    >
+    <Tabs.Screen  name="index" options={{title: 'Compare',headerShown: false}}/>
+    <Tabs.Screen  name="Profile" options={{headerShown: false}}/>
+  </Tabs>
+  )
 }
+
+export default _layout
+
+// const styles = StyleSheet.create({})
