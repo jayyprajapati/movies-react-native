697185cb3bde9862699dd64b5d424ddfef81b28a
Jay Prajapati <jay.prajapati5717@gmail.com>
2025-06-24 23:36:33 +0530

    feat: movies list api call

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Profile = () => {
   return (
-    <View>
+    <SafeAreaView> 
+      <View>
       <Text>Profile</Text>
     </View>
+    </SafeAreaView>
+    
   )
 }
 
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from "react";
-import { ActivityIndicator, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default function Index() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -10,7 +11,7 @@ export default function Index() {
       setLoading(true);
       const options = {
         method: 'GET',
-        url: 'https://api.themoviedb.org/3/account/22095472',
+        url: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
         headers: {
           accept: 'application/json',
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTBmMWY2MTg1YWY1ZmNiOWMxMDIxZDc4ZTBkMjE0OCIsIm5iZiI6MTc1MDY0NjU1MS4yMjgsInN1YiI6IjY4NThiZjE3MWRlNDFkODMyNjRiZDM3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.i1jJGo_g1_cbydgHJEKf7Gle6LbePK8wsytLTkoHtVg'
@@ -20,8 +21,8 @@ export default function Index() {
       try {
         const res = await axios.request(options);
         if (res.status === 200) {
-          setData(res.data);
-          console.log(res.data)
+          setData(res.data.results);
+          // console.log(res.data.results)
         } else {
           console.error("Error fetching data", res.status);
         }
@@ -41,8 +42,17 @@ export default function Index() {
         <ActivityIndicator />
       ) : data ? (
         <>
-          <Text>Username: {data.username}</Text>
-          <Text>Name: {data.name}</Text>
+        <SafeAreaView>
+          <ScrollView>
+          <View>
+            <FlatList data={data}  renderItem={({item}) => (<Text>{item.original_title}</Text>)} keyExtractor={(item) => item.id}/>
+          </View>
+        </ScrollView>
+        </SafeAreaView>
+        
+        
+          {/* <Text>Username: {data.username}</Text>
+          <Text>Name: {data.name}</Text> */}
         </>
       ) : (
         <Text>No data found</Text>
